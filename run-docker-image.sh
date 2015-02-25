e37bf9d50df8b0a71cb7bf48a7bb046ae477543c
Nicolas De Loof <nicolas.deloof@gmail.com>
2015-02-25 07:44:59 +0100

    make data directory configurable by env variable

--- run-docker-image.sh
+++ run-docker-image.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+DATA=${CFP_DATA:="$PWD/data"}
+
 # Global cleanup
 if [ $(docker ps -aq | wc -l) -gt 0 ]
 then
@@ -9,5 +11,5 @@ fi
 
 echo "Launching containers"
 docker run -d -ti --name cfp-elasticsearch -p 9200:9200 -p 9300:9300 -h elasticsearch dockerfile/elasticsearch
-docker run -d -ti --name cfp-redis -p 6379:6379 -v $PWD/data:/data -h redis redis
+docker run -d -ti --name cfp-redis -p 6379:6379 -v $DATA:/data -h redis redis
 docker run -d -ti --name cfp-webapp --link cfp-elasticsearch:elasticsearch --link cfp-redis:redis -p 80:9000 breizhcamp/cfp-webapp
